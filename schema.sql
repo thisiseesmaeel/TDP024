f26caf69fccff5fcaadde73b9c2ccb3115a021a4
Simon Lindblad <simon.lindblad93@gmail.com>
2016-05-11 20:33:21 +0200

    Add tasks to database schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,11 @@
+drop table if exists task cascade;
+create table task(
+    id serial primary key,
+    name text not null,
+    done boolean default false,
+    list int references list(id)
+);
+
 drop table if exists list cascade;
 create table list(
     id serial primary key,
