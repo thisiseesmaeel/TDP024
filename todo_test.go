ffc0a2077b1d3add01a0f2e888164d072ee92c69
Simon Lindblad <simon.lindblad93@gmail.com>
2016-05-11 20:38:32 +0200

    Add unit tests for task management

--- todo_test.go
+++ todo_test.go
@@ -6,6 +6,7 @@ import (
     "net/http/httptest"
     "testing"
     "strings"
+    "strconv"
     "encoding/json"
     "bytes"
 )
@@ -29,6 +30,68 @@ func setup(t *testing.T) {
         checkFail(t, err)
     }
 
+}
+
+func TestTask(test *testing.T) {
+    setup(test)
+    testServer := httptest.NewServer(Handlers())
+    defer testServer.Close()
+
+    b, err := json.Marshal(ListCreateRequest{Name: "Work"})
+    checkFail(test, err)
+
+    res, err := http.Post(testServer.URL + "/list", "application/json", bytes.NewReader(b))
+    checkFail(test, err)
+
+    listResp := ListCreateResponse{}
+    body, err := ioutil.ReadAll(res.Body)
+    defer res.Body.Close()
+    err = json.Unmarshal(body, &listResp)
+
+    res, err = http.Get(testServer.URL + "/list/" + strconv.Itoa(listResp.Id))
+    checkFail(test, err)
+
+    if res.StatusCode != 200 {
+        test.Errorf("Expected 200 got %d", res.StatusCode)
+    }
+
+    body, err = ioutil.ReadAll(res.Body)
+    defer res.Body.Close()
+
+    var tasks []Task
+    err = json.Unmarshal(body, &tasks)
+    checkFail(test, err)
+
+    if len(tasks) != 0 {
+        test.Errorf("Expected len(tasks) == 0 got %d", len(tasks))
+    }
+
+    b, err = json.Marshal(CreateTaskRequest{Name: "Work", ListId: listResp.Id})
+    checkFail(test, err)
+
+    res, err = http.Post(testServer.URL + "/task", "application/json", bytes.NewReader(b))
+    checkFail(test, err)
+
+    if res.StatusCode != 200 {
+        test.Errorf("Expected 200, got %d")
+    }
+
+    res, err = http.Get(testServer.URL + "/list/" + strconv.Itoa(listResp.Id))
+    checkFail(test, err)
+
+    if res.StatusCode != 200 {
+        test.Errorf("Expected 200 got %d", res.StatusCode)
+    }
+
+    body, err = ioutil.ReadAll(res.Body)
+    defer res.Body.Close()
+
+    err = json.Unmarshal(body, &tasks)
+    checkFail(test, err)
+
+    if len(tasks) != 1 {
+        test.Errorf("Expected len(tasks) == 1 got %d", len(tasks))
+    }
 }
 
 func TestList(test *testing.T) {
