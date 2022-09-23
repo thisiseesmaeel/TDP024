46887b753a89f8006dcc68d1e931736a7cf7564a
Hadi Ansari <hadan326@student.liu.se>
2022-09-23 14:21:40 +0200

    Simple rest and dependency injection in logic layer done

--- lab3/account-rest/src/main/java/se/liu/ida/tdp024/account/rest/AccountController.java
+++ lab3/account-rest/src/main/java/se/liu/ida/tdp024/account/rest/AccountController.java
@@ -3,26 +3,26 @@ package se.liu.ida.tdp024.account.rest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import se.liu.ida.tdp024.account.data.impl.db.facade.AccountEntityFacadeDB;
 import se.liu.ida.tdp024.account.logic.api.facade.AccountLogicFacade;
+import se.liu.ida.tdp024.account.logic.impl.facade.AccountLogicFacadeImpl;
+
 
 @RestController
 @RequestMapping("/account-rest")
 public class AccountController {
-    AccountLogicFacade accountLogicFacade;
-
-    public AccountController(AccountLogicFacade accountLogicFacade) {
-        this.accountLogicFacade = accountLogicFacade;
-    }
-
+    AccountLogicFacade accountLogicFacade = new AccountLogicFacadeImpl(new AccountEntityFacadeDB());
+        //this.accountLogicFacade = accountLogicFacade
+    
     @RequestMapping("/account/create")
     public String create(@RequestParam String accountType, @RequestParam String person, @RequestParam String bank) {
-        accountLogicFacade.create(accountType, person, bank);
+        //accountLogicFacade.create(accountType, person, bank);
         return "Create works!";
     }
 
     @RequestMapping("/account/find/person")
-    public String findPerson(@RequestParam(value="name", defaultValue="World") String name) {
-
+    public String findPerson(@RequestParam String personKey) {
+        //accountLogicFacade.find(personKey);
         return "Find person works!";
     }
 
--- lab3/account-rest/src/main/java/se/liu/ida/tdp024/account/rest/Application.java
+++ lab3/account-rest/src/main/java/se/liu/ida/tdp024/account/rest/Application.java
@@ -3,6 +3,7 @@ package se.liu.ida.tdp024.account.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 
 public class Application {
