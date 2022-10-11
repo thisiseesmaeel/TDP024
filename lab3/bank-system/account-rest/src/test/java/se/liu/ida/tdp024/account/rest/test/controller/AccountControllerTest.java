8417ebed78e16baa4ca2dae789086f545b644865
Hadi Ansari <hadan326@student.liu.se>
2022-10-11 09:18:25 +0200

    Configurated pom for "rest (web) layer" to support code coverage and added test template

---
+++ lab3/bank-system/account-rest/src/test/java/se/liu/ida/tdp024/account/rest/test/controller/AccountControllerTest.java
@@ -0,0 +1,12 @@
+package se.liu.ida.tdp024.account.rest.test.controller;
+
+import junit.framework.Assert;
+import org.junit.jupiter.api.Test;
+
+public class AccountControllerTest {
+
+    @Test
+    public void testUnnecessary(){
+        Assert.assertEquals(2 , 2);
+    }
+}
